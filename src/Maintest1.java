04c4b39245ff7fe0a5464deae31c3ee378fc1b71
ternet1978 <admin_radomilk@ukr.net>
2021-02-18 11:01:54 +0200

    Создал первый класс

---
+++ src/Maintest1.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 18.02.21
+ * Time: 10:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Maintest1 {
+    public static void main(String[] args) {
+        System.out.println("HeLLO!!!");
+        System.out.println("HeLLO!!!");
+        System.out.println("HeLLO!!!");
+
+    }
+}
